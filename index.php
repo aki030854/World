6919da6731930a4d515fc55e0122b0fe03525eef
akiko <annzu38@yahoo,co.jp>
2023-05-26 02:34:23 +0900

    block?

--- index.php
+++ index.php
@@ -1,16 +1,15 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>World Clock                     </title>
     <link rel="stylesheet" href="css/sanitize.css">
     <link rel="stylesheet" href="css/common.css">
     <link rel="stylesheet" href="css/index.css">   
 </head>
-
-
+                                              
 <body>
   <header class="header">
     <div class="header_inner">
@@ -19,6 +18,7 @@
         </a>
     </div>
   </header>
+
   <main>
   <div class="search-form_content">
      <div class="search-form__heading">
